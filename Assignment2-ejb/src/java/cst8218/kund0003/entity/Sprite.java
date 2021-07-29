ef157646129f3d602f8c913f90b1390f0242db83
JosefKundinger-Markhauser <joeymarkhauser@yahoo.ca>
2021-07-29 14:17:57 -0400

    Inital Setup

---
+++ Assignment2-ejb/src/java/cst8218/kund0003/entity/Sprite.java
@@ -0,0 +1,226 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cst8218.kund0003.entity;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.io.Serializable;
+import java.util.Random;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Joey Kundinger-Markhauser
+ * @version 1.0
+ * 
+ * This class is used to store the values of the Sprite object.
+ */
+@Entity
+@XmlRootElement
+public class Sprite implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public final static Random random = new Random();
+
+    final static int SIZE = 10;
+    final static int MAX_SPEED = 5;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    @Column
+    @Min(0)
+    private Integer panelWidth;
+    @Column
+    @Min(0)
+    private Integer panelHeight;
+    @Column
+    @Min(0)
+    private Integer x;
+    @Column
+    @Min(0)
+    private Integer y;
+    @Column
+    private Integer dx;
+    @Column
+    private Integer dy;
+    @Column
+    private Color color = Color.BLUE;
+
+    public Sprite() {
+    }
+
+    public Sprite(int height, int width) {
+        this.panelWidth = width;
+        this.panelHeight = height;
+        x = random.nextInt(width);
+        y = random.nextInt(height);
+        dx = random.nextInt(2 * MAX_SPEED) - MAX_SPEED;
+        dy = random.nextInt(2 * MAX_SPEED) - MAX_SPEED;
+    }
+
+    public Sprite(int height, int width, Color color) {
+        this(height, width);
+        this.color = color;
+    }
+    
+    /**
+     * Updates a different sprite with the non-zero 
+     * values from the current sprite.
+     * @param entity The entity to get values from.
+     */
+    public void updates(Sprite entity){
+        if(this.panelWidth != null){
+            entity.setPanelWidth(this.getPanelWidth());
+        }
+        if(this.panelHeight != null){
+            entity.setPanelHeight(this.getPanelHeight());
+        }
+        if(this.x != null){
+            entity.setX(this.getX());
+        }
+        if(this.y != null){
+            entity.setY(this.getY());
+        }
+        if(this.dx != null){
+            entity.setDx(this.getDx());
+        }
+        if(this.dy != null){
+            entity.setDy(this.getDy());
+        }
+    }
+
+    public int getPanelWidth() {
+        return panelWidth;
+    }
+
+    public void setPanelWidth(int panelWidth) {
+        this.panelWidth = panelWidth;
+    }
+
+    public int getPanelHeight() {
+        return panelHeight;
+    }
+
+    public void setPanelHeight(int panelHeight) {
+        this.panelHeight = panelHeight;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x, y, SIZE, SIZE);
+    }
+
+    public void move() {
+
+        // check for bounce and make the ball bounce if necessary
+        //
+        if (x < 0 && dx < 0) {
+            //bounce off the left wall 
+            x = 0;
+            dx = -dx;
+        }
+        if (y < 0 && dy < 0) {
+            //bounce off the top wall
+            y = 0;
+            dy = -dy;
+        }
+        if (x > panelWidth - SIZE && dx > 0) {
+            //bounce off the right wall
+            x = panelWidth - SIZE;
+            dx = -dx;
+        }
+        if (y > panelHeight - SIZE && dy > 0) {
+            //bounce off the bottom wall
+            y = panelHeight - SIZE;
+            dy = -dy;
+        }
+
+        //make the ball move
+        x += dx;
+        y += dy;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Sprite)) {
+            return false;
+        }
+        Sprite other = (Sprite) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Sprite[ id=" + id + " ]";
+    }
+
+}
---
+++ Assignment2-ejb/src/java/service/SpriteFacadeREST.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import cst8218.kund0003.entity.Sprite;
+import cst8218.kund0003.entity.AbstractFacade;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Joey Kundinger-Markhauser
+ * @version 1.0
+ * 
+ * This class implements a RESTFUL API.
+ */
+@javax.ejb.Stateless
+@javax.ws.rs.Path("cst8218.kund0003.entity.sprite")
+public class SpriteFacadeREST extends AbstractFacade<Sprite> {
+    
+    private String okMessage = "Success";
+
+    @PersistenceContext(unitName = "SpriteJoey-ejbPU")
+    private EntityManager em;
+
+    public SpriteFacadeREST() {
+        super(Sprite.class);
+    }
+
+    /**
+     * 
+     * @param id The ID of the Sprite to be operated on.
+     * @param newSprite The new Sprite object passed through the request.
+     * @return Error response message when an error occurs, or okay message when successful.
+     */
+    @javax.ws.rs.PUT
+    @javax.ws.rs.Path("{id}")
+    @javax.ws.rs.Consumes({javax.ws.rs.core.MediaType.APPLICATION_XML, javax.ws.rs.core.MediaType.APPLICATION_JSON})
+    public Response put(@javax.ws.rs.PathParam("id") Long id, Sprite newSprite) {
+        Sprite oldSprite = super.find(id);
+        if (oldSprite == null){
+            return Response.status(404).build();
+        }
+        else if(!Objects.equals(oldSprite.getId(), newSprite.getId())){
+            return Response.status(403).build();
+        }
+        super.edit(newSprite);
+       
+        return Response.ok(okMessage).build();
+    }
+    
+    /**
+     * 
+     * @param id The ID of the Sprite to be operated on.
+     * @param newSprite The new Sprite object passed through the request.
+     * @return Error response message when an error occurs, or okay message when successful.
+     */
+    @javax.ws.rs.POST
+    @javax.ws.rs.Path("{id}")
+    @javax.ws.rs.Consumes({javax.ws.rs.core.MediaType.APPLICATION_XML, javax.ws.rs.core.MediaType.APPLICATION_JSON})
+    public Response post(@javax.ws.rs.PathParam("id") Long id, Sprite newSprite) {
+        Sprite oldSprite = super.find(id);
+        if (oldSprite == null){
+            return Response.status(404).build();
+        }
+        else if(!Objects.equals(oldSprite.getId(), newSprite.getId())){
+            return Response.status(403).build();
+        }
+        
+        newSprite.updates(oldSprite);
+        super.edit(oldSprite);
+        
+        return Response.ok(okMessage).build();
+    }
+    
+    /**
+     * 
+     * @param newSprite The new Sprite object passed through the request.
+     * @return Error response message when an error occurs, or okay message when successful.
+     */
+    @javax.ws.rs.POST
+    @javax.ws.rs.Consumes({javax.ws.rs.core.MediaType.APPLICATION_XML, javax.ws.rs.core.MediaType.APPLICATION_JSON})
+    public Response post(Sprite newSprite) {
+        if(newSprite.getId() == null){
+            super.create(newSprite);
+        }
+        else{
+            Sprite oldSprite = super.find(newSprite.getId());
+            if(oldSprite != null){
+                newSprite.updates(oldSprite);
+                super.edit(oldSprite);
+            }
+            else{
+                return Response.status(404).build();
+            }
+        }
+        
+        return Response.ok(okMessage).build();
+    }
+
+    @javax.ws.rs.DELETE
+    @javax.ws.rs.Path("{id}")
+    public void remove(@javax.ws.rs.PathParam("id") Long id) {
+        super.remove(super.find(id));
+    }
+
+    @javax.ws.rs.GET
+    @javax.ws.rs.Path("{id}")
+    @javax.ws.rs.Produces({javax.ws.rs.core.MediaType.APPLICATION_XML, javax.ws.rs.core.MediaType.APPLICATION_JSON})
+    public Sprite find(@javax.ws.rs.PathParam("id") Long id) {
+        return super.find(id);
+    }
+
+    @javax.ws.rs.GET
+    @Override
+    @javax.ws.rs.Produces({javax.ws.rs.core.MediaType.APPLICATION_XML, javax.ws.rs.core.MediaType.APPLICATION_JSON})
+    public List<Sprite> findAll() {
+        return super.findAll();
+    }
+
+    @javax.ws.rs.GET
+    @javax.ws.rs.Path("{from}/{to}")
+    @javax.ws.rs.Produces({javax.ws.rs.core.MediaType.APPLICATION_XML, javax.ws.rs.core.MediaType.APPLICATION_JSON})
+    public List<Sprite> findRange(@javax.ws.rs.PathParam("from") Integer from, @javax.ws.rs.PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    /**
+     * This is the endpoint to see how many sprites are in the database.
+     * @return The total number of Sprites.
+     */
+    @javax.ws.rs.GET
+    @javax.ws.rs.Path("count")
+    @javax.ws.rs.Produces(javax.ws.rs.core.MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
