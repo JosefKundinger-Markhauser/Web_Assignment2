c8c9eeeb2de0e7696c4679927f0258616f258421
JosefKundinger-Markhauser <joeymarkhauser@yahoo.ca>
2021-08-05 16:31:48 -0400

    JUNIT testing implemented for appuser and sprite

---
+++ Assignment2-ejb/Testing Packages/tests/AppUserTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import ass2.entity.AppUser;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Joey Kundinger-Markhauser
+ * @author Patrick Czermak
+ */
+public class AppUserTest {
+
+    public AppUserTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void hello() {
+        System.out.println("Testing stuff");
+        AppUser user = new AppUser();
+    }
+    
+    @Test
+    public void userIdTest() {
+        System.out.println("Testing userId");
+        AppUser user = new AppUser();
+        String setTest = "testUserId";
+        user.setUserId(setTest);
+        String getTest = user.getUserId();
+        assertTrue(setTest == getTest);
+    }
+    
+    @Test
+    public void groupNameTest() {
+        System.out.println("Testing groupName");
+        AppUser user = new AppUser();
+        String setTest = "groupName";
+        user.setGroupName(setTest);
+        String getTest = user.getGroupName();
+        assertTrue(setTest == getTest);
+    }
+}
